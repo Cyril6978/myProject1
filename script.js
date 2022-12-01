eaa970fed7094b7c5f98bf00bce3ab43aace3600
Cyril6978 <cyril.david8978@gmail.com>
2022-12-01 16:29:33 +0100

    js en cours

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("connecté");
+
+// click sur burger = ouverture navbar
+const icon = document.querySelector(".fa-bars");
+const list = document.querySelector(".navlist_mobile");
+
+icon.addEventListener("click", function() {
+    list.classList.toggle("list-visible");
+    icon.classList.toggle("icon-visible");
+        console.log(list);
+    })
